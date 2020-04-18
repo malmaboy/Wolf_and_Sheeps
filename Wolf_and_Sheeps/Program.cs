0deb5ad8d9c68938bf476203e93ad38d50e2892d
Hugo Carvalho <huguinho66@live.com>
2020-04-18 05:22:42 +0100

    Symbols Unfinished

--- Wolf_and_Sheeps/Program.cs
+++ Wolf_and_Sheeps/Program.cs
@@ -10,32 +10,5 @@ namespace Wolf_and_Sheeps
             Board.DisplayBoard();
         }
 
-/*        static void Board() // Desenha o tabuleiro
-        {
-            string horizontalbar = "----------------";
-            string verticalbar = "| ";
-            string  Numbers = ("1 2 3 4 5 6 7 8");
-            string letters = ("A B C D E F G H");
-            int lins = 8, cols = 8, i, j;
-            int[,] m = {  {1,2,3,4,5,6,7,8},
-                                            {1,2,3,4,5,6,7,8},
-                                            {1,2,3,4,5,6,7,8},
-                                            {1,2,3,4,5,6,7,8},
-                                            {1,2,3,4,5,6,7,8},
-                                            {1,2,3,4,5,6,7,8},
-                                            {1,2,3,4,5,6,7,8},
-                                            {1,2,3,4,5,6,7,8}, };
-            
-          Console.WriteLine(Numbers);
-            for (i = 0; i < lins; i++)
-            {
-                Console.WriteLine(horizontalbar);
-                for (j = 0; j < cols; j++)
-                    Console.Write("{0} ", m[i, j]);
-                    Console.WriteLine(verticalbar);
-                    
-            }
-        }*/
-
     }
 }
--- Wolf_and_Sheeps/Symbols.cs
+++ Wolf_and_Sheeps/Symbols.cs
@@ -5,17 +5,97 @@ namespace Wolf_and_Sheeps
 {
     public class Symbols
     {
-        public const char X_symbol = 'X'; // Wolf Symbol
+        public const char X_symbol = 'X'; // Simbolo do Lobo
         public const char Space = ' ';
         public static char [,] symbols;
-		public const char O_symbol = 'O'; // Sheep symbol
+		public const char O_symbol = 'O'; // Simbolo da Ovelha
 
+        private string Escolha; 
         public Symbols()
         {
             symbols = new char [BOARD.Dimension, BOARD.Dimension];
-            
+            //InicialSymbols();
+            InicialWolfs();
+            InicialSheeps();
         } 
 
+     /*   public void InicialSymbols()
+        {
+            for(int l = 0;l < BOARD.Dimension;l++)
+            {
+                for(int c = 4; c < BOARD.Dimension; c++)
+                {
+                    if (l == 0 || l == 0 || l == BOARD.Dimension - 1 || l == BOARD.Dimension - 4)
+                        symbols[l,c] = X_symbol;
+                    else
+                        symbols[l,c] = Space;
+                    
+                }
+
+                    
+            }
+        } */
+
+        
+
+
+        public void InicialWolfs() // Desenha as peças de acordo com a escolha do jogador
+        {
+            do
+            {
+                Console.WriteLine("Escolha a posição do Lobo. ");
+                Escolha = Console.ReadLine();
+                
+
+                switch(Escolha)
+                {
+                    case "0":
+                        Console.WriteLine("1");
+                        break;
+                    case "1": 
+                        Console.WriteLine(BOARD.Dimension(X_symbol,Space));
+                        break;
+                    case "2":
+                        Console.WriteLine("1");
+                        break;
+                    case "3":   
+                        Console.WriteLine("1");
+                        break;
+                    case "4":
+                        Console.WriteLine("1");
+                        break;
+                    case "5":
+                        Console.WriteLine("1");
+                        break;
+                    case "6":
+                        Console.WriteLine("1");
+                        break;
+                    case "7":
+                        Console.WriteLine("1");
+                        break;
+                    default:
+                        Console.WriteLine("Opção Invalida, o número tem de ser 1 a 7");
+                        break;
+                }   
+            }while(false);
+        }
+
+        public void InicialSheeps()
+        {
+            
+        }
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
 
     }
 }
